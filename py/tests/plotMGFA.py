f760f22685aadcc59929220bd273e3b831c8e4ad
Anze Slosar <anze@bnl.gov>
2015-08-03 16:59:38 +0100

    nicer plot

--- py/tests/plotMGFA.py
+++ py/tests/plotMGFA.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python
 import sys
+import numpy as np
+
 sys.path.append("../etc")
 sys.path.append("etc")
 
 from MockGFA import MockGFA
 import pylab
 
-gfagp=MockGFA(Nstars=4, Nskies=4,star_rmags=17).Yield(0.0)
-for i in range(4):
-    pylab.subplot(2,4,1+i)
-    pylab.imshow(gfagp.stars[i].array)
-    pylab.subplot(2,4,5+i)
-    pylab.imshow(gfagp.skies[i].array)
+rmags=np.linspace(18,15,8)
+gfagp=MockGFA(Nstars=8, Nskies=8,star_rmags=rmags).Yield(0.0)
 
+mn=min([x.array.min() for x in gfagp.stars+gfagp.skies])
+mx=max([x.array.max() for x in gfagp.stars+gfagp.skies])
+
+pylab.figure(figsize=(12,12))
+for i in range(8):
+    pylab.subplot(4,4,1+i)
+    pylab.imshow(gfagp.stars[i].array,vmin=mn,vmax=mx,interpolation='nearest', cmap=pylab.cm.binary)
+    pylab.xlabel('r=%g'%rmags[i])
+    pylab.subplot(4,4,9+i)
+    pylab.imshow(gfagp.skies[i].array,vmin=mn,vmax=mx,interpolation='nearest',cmap=pylab.cm.binary)
+    pylab.xlabel('sky')
 pylab.show()
 
