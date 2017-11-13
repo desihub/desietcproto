6a3597038316a91627979944013eb56c97dacac4
David Kirkby <dkirkby@uci.edu>
2017-11-13 13:33:58 -0800

    Add saving plot to unittest

--- py/desietc/test/test_plots.py
+++ py/desietc/test/test_plots.py
@@ -1,6 +1,9 @@
 from __future__ import print_function, division
 
 import unittest
+import os
+import tempfile
+import shutil
 
 import desietc.calculator
 
@@ -12,6 +15,16 @@ matplotlib.use('Agg') # Must be before importing matplotlib.pyplot or pylab!
 
 class TestPlots(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        # Create a temporary directory.
+        cls.tmpdir = tempfile.mkdtemp()
+
+    @classmethod
+    def tearDownClass(cls):
+        # Remove the directory after the test.
+        shutil.rmtree(cls.tmpdir)
+
     def test_calculator_plot(self):
         """Test that we can create a calculator and plot its state"""
         calc = desietc.calculator.Calculator(
@@ -21,4 +34,6 @@ class TestPlots(unittest.TestCase):
             0., 10.)
         calc.update_signal(1000., 1., 0.1)
         calc.update_background(900., 0.5, 0.2)
-        plot_calculator(calc, tnow=1500.)
+        # Save plot to temporary directory.
+        save = os.path.join(TestPlots.tmpdir, 'test.png')
+        plot_calculator(calc, tnow=1500., nsamples=10, save=save)
